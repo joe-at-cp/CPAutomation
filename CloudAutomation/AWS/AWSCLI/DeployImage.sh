2f33c48c6f0b913e5e839e97ec59eb2b972a590b
joe-at-cp <dilligj@checkpoint.com>
2018-07-17 12:00:24 -0500

    new file:   Ansible/CheckPoint/gw_clish_set_dns.yml 	new file:   Ansible/CheckPoint/gw_mgmt_clish_set_dns.yml 	new file:   CloudAutomation/AWS/AWSCLI/CreateEnvironment.sh 	new file:   CloudAutomation/AWS/AWSCLI/describe-instances 	new file:   CloudAutomation/AWS/AWSCLI/describe-vpcs 	new file:   GenericAPI/Bash/show-object-by-uuid 	new file:   GenericAPI/Bash/show-objects 	renamed:    Install_Ansible.sh -> Install_Dependencies.sh

---
+++ CloudAutomation/AWS/AWSCLI/DeployImage.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+
+#Check Point Automation Training
+
+#Create VPC
+echo '[+] Creating VPC'
+VPCID=`aws ec2 create-vpc --cidr-block 10.0.0.0/16 | ./jq-linux64 '.Vpc.VpcId' | sed -e 's/^"//' -e 's/"$//'`
+echo '[-]   '$VPCID
+sleep 2s
+
+#Create Subnet In VPC
+echo '[+] Creating Subnet inside VPC '$VPCID
+SUBNETID=`aws ec2 create-subnet --vpc-id $VPCID --cidr-block 10.0.1.0/24 | ./jq-linux64 '.Subnet.SubnetId' | sed -e 's/^"//' -e 's/"$//'`
+echo '[-]   '$SUBNETID
+
+#Create Subnet In VPC
+echo '[+] Creating Internet Gateway'
+INETGW=`aws ec2 create-internet-gateway | ./jq-linux64 '.InternetGateway.InternetGatewayId' | sed -e 's/^"//' -e 's/"$//'`
+echo '[-]   '$INETGW
+
+#Add Internet Gateway to VPC
+echo '[+] Assigning Internet Gateway to VPC'
+aws ec2 attach-internet-gateway --internet-gateway-id $INETGW --vpc-id $VPCID > /dev/null
+
+#Create Security Group
+echo '[+] Creating New Security Group'
+SECGROUP=`aws ec2 create-security-group --group-name AutomationSecurityGroup --description "AutomationGroup" --vpc-id $VPCID | ./jq-linux64 '.GroupId' | sed -e 's/^"//' -e 's/"$//'`
+echo '[-]   '$SECGROUP
+
+#Add Rules into Security Group
+echo '[+] Adding Inbound Rules To  Security Group'
+aws ec2 authorize-security-group-ingress --group-id $SECGROUP --protocol tcp --port 22 --cidr 0.0.0.0/0
+aws ec2 authorize-security-group-ingress --group-id $SECGROUP --protocol tcp --port 80 --cidr 0.0.0.0/0
+aws ec2 authorize-security-group-ingress --group-id $SECGROUP --protocol tcp --port 5901 --cidr 0.0.0.0/0
+aws ec2 authorize-security-group-ingress --group-id $SECGROUP --protocol icmp --port -1 --cidr 0.0.0.0/0
+
+#Gather VPC Subnet ID
+SUBNETID=`aws ec2 describe-subnets --filters "Name=vpc-id,Values=$VPCID" --query 'Subnets[*].{ID:SubnetId,CIDR:CidrBlock}' | ./jq-linux64 '.[] .ID' | sed -e 's/^"//' -e 's/"$//'`
+
+#Create Routing Table
+echo '[+] Creating Route Table'
+ROUTETABLE=`aws ec2 create-route-table --vpc-id $VPCID | ./jq-linux64 '.RouteTable.RouteTableId' | sed -e 's/^"//' -e 's/"$//'`
+echo '[-]   '$ROUTETABLE
+
+#Create Default Route
+echo '[+] Creating Default Route'
+aws ec2 create-route --route-table-id $ROUTETABLE --destination-cidr-block 0.0.0.0/0 --gateway-id $INETGW > /dev/null
+
+#Associate Routing Table
+echo '[+] Associate Routing Table To VPC Subnet'
+aws ec2 associate-route-table  --subnet-id $SUBNETID --route-table-id $ROUTETABLE > /dev/null
+
+#Launch Image and Get InstanceId in Return
+echo '[+] Deploying AWS Image'
+InstanceID=`aws ec2 run-instances --image-id ami-ea846afc --count 1 --instance-type t2.micro --security-group-ids $SECGROUP --key-name AutomationTraining --subnet-id $SUBNETID |  ./jq-linux64 '.Instances[0] .InstanceId' | sed -e 's/^"//' -e 's/"$//'`
+echo '[-]   Instance Id: '$InstanceID
+
+#Allowcate public address and store it
+echo '[+] Allowcating Public IP'
+IPRETURN=`aws ec2 allocate-address`
+PublicIp=`echo $IPRETURN | ./jq-linux64 '.PublicIp' | sed -e 's/^"//' -e 's/"$//'`
+IPALLOC=`echo $IPRETURN | ./jq-linux64 '.AllocationId' | sed -e 's/^"//' -e 's/"$//'`
+echo '[-]   Public IP: '$PublicIp
+echo '[-]   Allocation Id: '$IPALLOC
+
+echo '[+] Waiting for Instance to Startup...'
+
+sleep 25s
+
+#Asign public ip to instance
+echo '[+] Assigning Public Ip to Instance...'
+ASSOCID=`aws ec2 associate-address --instance-id $InstanceID --public-ip $PublicIp | ./jq-linux64 '.AssociationId' | sed -e 's/^"//' -e 's/"$//'`
+echo '[+] Complete!'
+
+echo '[+] Writing .VNC File'
+echo '[connection]' > Desktop.vnc
+echo 'host='$PublicIp >> Desktop.vnc
+echo 'port=5901' >> Desktop.vnc
+echo 'password='$InstanceID >> Desktop.vnc
+
+echo ''
+echo 'When ready to delete, hit ENTER...'
+read input
+
+echo '[+] Deleteing AWS Environment!'
+
+echo '[!] Disassoicating Public IP'
+aws ec2 disassociate-address --association-id $ASSOCID
+
+sleep 3s
+echo '[!] Releasing Public IP'
+aws ec2 release-address --allocation-id $IPALLOC
+
+sleep 3s
+echo '[!] Terminating Instance...'
+aws ec2 terminate-instances --instance-ids $InstanceID > /dev/null
+
+sleep 2m
+echo '[!] Deleting Security Group'
+aws ec2 delete-security-group --group-id $SECGROUP
+
+echo '[!] Deleting Subnet'
+aws ec2 delete-subnet --subnet-id $SUBNETID
+sleep 3s
+
+echo '[!] Deleting Route Table'
+aws ec2 delete-route-table --route-table-id $ROUTETABLE
+
+echo '[!] Detach Internet Gateway From VPC'
+aws ec2 detach-internet-gateway --internet-gateway-id $INETGW --vpc-id $VPCID
+
+echo '[!] Deleting Internet Gateway'
+aws ec2 delete-internet-gateway --internet-gateway-id $INETGW
+
+echo '[!] Deleting VPC'
+aws ec2 delete-vpc --vpc-id $VPCID
--- Install_Dependencies.sh
+++ Install_Dependencies.sh
@@ -4,11 +4,35 @@
 #Ansible Install and Configuration Script
 
 clear
-echo "==== Installing and Configuring Ansible ===="
+echo "=========== Automation Server Installation Utility ==========="
+echo " This utlity will install the following tools:"
+echo "- Ansible"
+echo "- AWS CLI"
+echo "- Azure CLI"
+echo ""
 
-#Update and download packages
+
+
+
+#Update Sources List
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
+    sudo tee /etc/apt/sources.list.d/azure-cli.list
+	
+curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+
+
+
+#APT update and download packages
 sudo apt-get update
-sudo apt-get install -y ansible git
+sudo apt-get install -y ansible apt-transport-https azure-cli
+
+#PIP Install Libs
+pip install awscli --upgrade --user
+
+
+clear
+echo "==== Installing and Configuring Ansible ===="
 
 #Clone Check Point Ansible libs
 git clone --recursive https://github.com/CheckPoint-APIs-Team/cpAnsible
@@ -27,4 +51,7 @@ rm -rf cpAnsible/
 
 #Create SSH Key
 ssh-keygen -t rsa -f $HOME/.ssh/id_rsa -N ''
+
+
+
 
