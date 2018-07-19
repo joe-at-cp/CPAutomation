4a9ee2d9e177dcd52b38240f57e78de98cc67001
joe-at-cp <dilligj@checkpoint.com>
2018-07-19 12:16:27 -0500

    deleted:    CloudAutomation/AWS/AWSCLI/CreateEnvironment.sh 	renamed:    CloudAutomation/AWS/AWSCLI/DeployImage.sh -> CloudAutomation/AWS/AWSCLI/aws-create-environment 	renamed:    CloudAutomation/AWS/AWSCLI/describe-instances -> CloudAutomation/AWS/AWSCLI/aws-ec2-describe-instances 	renamed:    CloudAutomation/AWS/AWSCLI/describe-vpcs -> CloudAutomation/AWS/AWSCLI/aws-ec2-describe-vpcs 	deleted:    CloudAutomation/Azure/AzureCLI/output 	new file:   CloudAutomation/Azure/Bash/add-host 	modified:   IdentityAPI/Bash/TORBlock.sh 	modified:   IdentityAPI/Bash/add-identity

--- IdentityAPI/Bash/TORBlock.sh
+++ IdentityAPI/Bash/TORBlock.sh
@@ -17,7 +17,7 @@ echo "Creating $nodecount Exit Node Identities..."
 sleep 5
 while read i; do
 
-        curl -s -k -H "Content-Type: application/json" -X POST -d'{"shared-secret":"'$cp_gw_ia_pass'","ip-address":"'$i'","machine":"TORExitNode","identity-source":"TOR Exit Node Script","domain":"Production Domain","calculate-roles":0,"session-timeout":86400,"fetch-machine-groups":0,"roles":["'$cp_gw_role'"]}' $cp_gw_ia_url/add-identity | ./jq-linux64        
+        curl -s -k -H "Content-Type: application/json" -X POST -d'{"shared-secret":"'$cp_gw_ia_pass'","ip-address":"'$i'","machine":"TORExitNode","identity-source":"TOR Exit Node Script","domain":"Production Domain","calculate-roles":1,"session-timeout":86400,"fetch-machine-groups":0,"roles":["'$cp_gw_role'"]}' $cp_gw_ia_url/add-identity | ./jq-linux64        
 done <NodeListCut.txt
 
 rm NodeListCut.txt
