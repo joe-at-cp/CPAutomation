f587ac7e37a6bcb7dfc929efc7038031db2ee67a
joe-at-cp <dilligj@checkpoint.com>
2018-09-14 10:54:47 -0500

    modified:   Install_Dependencies.sh

--- Install_Dependencies.sh
+++ Install_Dependencies.sh
@@ -10,7 +10,10 @@ echo "- Ansible"
 echo "- AWS CLI"
 echo "- Azure CLI"
 echo "- NTP"
+echo "- MSF Console"
+echo "- Check Point Ansible Libraries"
 
+sleep 10s
 
 #Update Sources List
 AZ_REPO=$(lsb_release -cs)
@@ -18,8 +21,6 @@ echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO 
     sudo tee /etc/apt/sources.list.d/azure-cli.list
 	
 curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-
-
 
 #APT update and download packages
 sudo apt-get install software-properties-common
@@ -40,24 +41,15 @@ echo "==== Installing and Configuring Ansible ===="
 
 #Clone Check Point Ansible libs
 git clone --recursive https://github.com/CheckPoint-APIs-Team/cpAnsible
-#sudo mkdir //ansible/modules
 sudo cp -r cpAnsible/check_point_mgmt/ /usr/lib/python2.7/dist-packages/ansible/
 sudo cp -r cpAnsible/check_point_mgmt/cp_mgmt_api_python_sdk/ /usr/lib/python2.7/dist-packages/
 
-sudo mkdir $HOME/.ansible
-sudo mkdir $HOME/.ansible/plugins
-sudo mkdir $HOME/.ansible/plugins/modules
+sudo mkdir -p $HOME/.ansible/plugins/modules
 sudo cp -r cpAnsible/check_point_mgmt/ $HOME/.ansible/plugins/modules/
 
-
 #/etc/ansible/ansible.cfg configuration
-#sudo echo 'library = $HOME/ansible/plugins/' >> /etc/ansible/ansible.cfg
 sudo echo 'scp_if_ssh = True' >> /etc/ansible/ansible.cfg
 sudo echo 'host_key_checking = False' >> /etc/ansible/ansible.cfg
-
-
-#Remove Files
-#rm -rf cpAnsible/
 
 #Create SSH Key
 #ssh-keygen -t rsa -f $HOME/.ssh/id_rsa -N ''
